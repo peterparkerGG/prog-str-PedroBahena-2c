d46fa8904e1cb945cd7838ff461c8386a0def0f3
Gael Bahena <20253ds072@utez.edu.mx>
2026-01-15 17:30:31 -0600

    chore: base actividad git merge withoutconflict

--- MergeWithOutConflict/src/Main.java
+++ MergeWithOutConflict/src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Semana 2");
         System.out.println("Versión base");
-        System.out.println("2 + 2 = 4");
-        System.out.println("5 * 3 = 15");
+
     }
 }
