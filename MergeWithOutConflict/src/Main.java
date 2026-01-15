3fcc2e53a8e6b597f37eb72dd1ae8207b4a44cfe
Gael Bahena <20253ds072@utez.edu.mx>
2026-01-15 17:32:16 -0600

    feat: agregar operaciones

--- MergeWithOutConflict/src/Main.java
+++ MergeWithOutConflict/src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Semana 2");
-        System.out.println("Versión base");
         System.out.println("2 + 2 = 4");
-        System.out.println("5 * 3 = 15.");
+        System.out.println("5 * 3 = 15");
+
     }
 }
