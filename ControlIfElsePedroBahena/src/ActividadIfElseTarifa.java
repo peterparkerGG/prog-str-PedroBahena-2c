d287ab4d5d63ba57254c657263490976b3f54e29
Gael Bahena <20253ds072@utez.edu.mx>
2026-01-28 18:51:18 -0600

    Trabajo tarifa else if

---
+++ ControlIfElsePedroBahena/src/ActividadIfElseTarifa.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class ActividadIfElseTarifa {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        int edad;
+        double tarifa = 0;
+        boolean esEstudiante;
+        System.out.println("Ingrese su edad:");
+        edad = leer.nextInt();
+        if (edad < 0 || edad > 120) {
+            System.out.println("Edad inválida");
+            System.exit(0);
+        }
+        System.out.println("Eres estudiante? (true/false):");
+        esEstudiante = leer.nextBoolean();
+        if (edad < 12) {
+            tarifa = 50;
+        }
+        else if (edad >= 12 && edad <= 17) {
+            if (esEstudiante) {
+                tarifa = 60;
+            } else {
+                tarifa = 80;
+            }
+        }
+        else if (edad >= 18) {
+            if (esEstudiante) {
+                tarifa = 90;
+            } else {
+                tarifa = 120;
+            }
+        }
+        System.out.println("Edad ingresada: " + edad);
+        System.out.println("Es estudiante?: " + esEstudiante);
+        System.out.println("Tarifa final:$" + tarifa);
+    }
+}
